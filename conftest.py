ff35e9bef9d3e3f607131b7ffb451589e1c00ac9
JattMones <downhillsailor@gmail.com>
2021-05-31 20:30:37 -0400

    adding some considerations/todo's. Removing warnings

--- conftest.py
+++ conftest.py
@@ -1,10 +1,45 @@
+""" Test Suite Configuration:
+Key Functions:
+`pytest_generate_tests(metafunc)` works to generate our tests with specific 'fixtures' or arangements/inputs (ie. run all tests 5 times)
+`pytest_addoption` works to set-up our custom flags and test config values found in the .ini file
+`pytest_configure` is responsible for reading config/.ini vars and setting them to be used by the suite (use of helper functions may be useful here for suites with many configuration areas)
+
+GLOBAL VARIABLES:
+PYTEST_ENV - global var holds the env our test suite should be run in
+PYTEST_SUITE_INFO - global var holds info related to running our test (time, os, connectivity, etc.)
+
+THINGS TO CONSIDER:
+Logging & console output?
+- Ideally we would remove the PYTEST_SUITE_INFO dict and have it's conntents be info level logs that are printed at the beginning with pytest standard output
+- ^ We should consider using a with statement so that pytest doesn't print all INFO related logs that could occur
+- Consider using the `record_testsuite_property("ARCH", "PPC")` to add the specific logging we want to be captured in a file.
+Hooks vs. Fixtures vs. helper functions?
+- Hooks are directly related to test configuration, because of this we should consider using hooks over fixtures in this conftest file
+- ^ ie. use the two separate set-up and tear-down hooks rather than our combined set_up_tear_down fixture
+- Helper functions should be used to group code, but again we should try to use the pre-defined hooks (or perhaps create custom hooks) rather than use many helper fucntions and fixtures
+Fixtures vs Gloabl Vars?
+- Fixtures do provide a space separate from the global name space
+- However, if a fixture is simply setting a single value/var to be used (and not completing some task/process), global vars may be a simpler way for our tests to access/be passed these values
+Flags to use when running?
+- depends on how we design our output
+-^ Ideally we get rid of print statements to report and are able to use logging (no need for -s or -rP flags)
+- On debuggign failures, -rx flag should be added to print captured output from failed tests while debugging.
+- `--verbose` may be useful for noobies using pytest, as it gives a non-abbreviated output format.
+- `--resultlog=path` should be used if we configure our `specific_info` to be recorded with `record_testsuite_property()`
+
+
+"""
 import pytest, os, sys, pendulum, pyspeedtest, psutil
 from pathlib import Path
 
 
 def pytest_addoption(parser):
     """ Special function adds custom flags to pytest arg """
-    parser.addini('env', help='valid envs include ()')
+    parser.addini('env', help='sets the env for our test suite')
+    parser.addini('valid_envs', help='defines valid envs for our test suite')
+    parser.addini('browser', help='sets browser for our test suite (all, chrome, firefox, safari)')
+    parser.addini('username', help='sets the username for our test suite (used for logins/requests)')
+    parser.addini('password', help='sets the password for our test suite (used for logins/requests)')
     parser.addoption("--repeat", action="store", help="enter an integer")
     parser.addoption("--env", action="store", help="enter an env ()")
 
@@ -13,13 +48,13 @@ def pytest_configure(config):
     config.addinivalue_line(
         "markers",
         "single_run: mark test to only run once"
-    )
+    )#TODO Should we consider just adding this directly to .ini
     set_env(config)
 
 def set_env(config):
     """ Sets our test env """
     global PYTEST_ENV
-    valid_envs = {'valid', 'env', 'set'}
+    valid_envs = config.getini('valid_envs')
     cmd_env = config.getoption("--env") # Commandline env var
     if cmd_env is not None:
         if cmd_env:
@@ -54,22 +89,23 @@ def get_ini(key, config):
 
 def general_info():
     """ Information we'd like to display with existing pytest output """
-    global pytest_suite_info
-    pytest_suite_info = {}
-    pytest_suite_info['dir_path'] = Path(__file__).parent.absolute()
-    pytest_suite_info['cwd'] = Path().absolute()
-    # pytest_suite_info['suite_root_path'] = Config.rootpath
-    # pytest_suite_info['suite_ini_file'] = Config.inipath
-    pytest_suite_info['os'] = sys.platform
-    pytest_suite_info['start_time'] = pendulum.now('America/Toronto')
+    global PYTEST_SUITE_INFO
+    PYTEST_SUITE_INFO = {}
+    PYTEST_SUITE_INFO['dir_path'] = Path(__file__).parent.absolute()
+    PYTEST_SUITE_INFO['cwd'] = Path().absolute()
+    # PYTEST_SUITE_INFO['suite_root_path'] = Config.rootpath
+    # PYTEST_SUITE_INFO['suite_ini_file'] = Config.inipath
+    PYTEST_SUITE_INFO['os'] = sys.platform
+    PYTEST_SUITE_INFO['start_time'] = pendulum.now('America/Toronto')
     st = pyspeedtest.SpeedTest('www.speedtest.net')
-    pytest_suite_info['ping'] = st.ping()
+    PYTEST_SUITE_INFO['ping'] = st.ping()
 
 def specific_info():
     """ Information which may be useful to look at if we notice odd/inconsistent test results """
-    global pytest_suite_info
+    #TODO consider using
+    global PYTEST_SUITE_INFO
     try:
-        f = open(str(Path('test_logs/'+str(pytest_suite_info['start_time']))), 'w+') # Better if this was being stored on a shared drive
+        f = open(str(Path('test_logs/'+str(PYTEST_SUITE_INFO['start_time']))), 'w+') # Better if this was being stored on a shared drive
         content = ''
     # content = psutil.....
     #measures things like memory, cpu, connectivity type, etc.
@@ -87,7 +123,7 @@ def setup_teardown():
     general_info()
     specific_info()
     yield #---- Start Teardown ----
-    global pytest_suite_info
-    pytest_suite_info['end_time'] = pendulum.now('America/Toronto')
-    for k, v in pytest_suite_info.items():
+    global PYTEST_SUITE_INFO
+    PYTEST_SUITE_INFO['end_time'] = pendulum.now('America/Toronto')
+    for k, v in PYTEST_SUITE_INFO.items():
         print(k, v)
